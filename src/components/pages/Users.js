6aac1e79d9e1c82505bf0312891f1f8896210681
Maxik-svg <60792664+Maxik-svg@users.noreply.github.com>
2020-11-13 02:46:55 +0200

    some styling added

--- src/components/pages/Users.js
+++ src/components/pages/Users.js
@@ -7,7 +7,7 @@ class Users extends Component {
     this.state = {
       result: [],
       idForUpdate: null,
-      updatingDbStarted : false,
+      updatingDbStarted: false,
     }
   }
 
@@ -20,24 +20,24 @@ class Users extends Component {
   }
 
   renderTableHeader() {
-    if(this.state.result[0] !== undefined){
+    if (this.state.result[0] !== undefined) {
       const {id, name, email, password, info, braceletId} = this.state.result[0]
-        return (
-          <React.Fragment>
-            <th>{"id"}</th>
-            <th>{"name"}</th>
-            <th>{"email"}</th>
-            <th>{"password"}</th>
-            <th>{"info"}</th>
-            <th>{"braceletId"}</th>
-          </React.Fragment>
+      return (
+        <React.Fragment>
+          <th>{"id"}</th>
+          <th>{"name"}</th>
+          <th>{"email"}</th>
+          <th>{"password"}</th>
+          <th>{"info"}</th>
+          <th>{"braceletId"}</th>
+        </React.Fragment>
       )
     }
   }
 
   renderTableData = () => {
     return this.state.result.map((user, index) => {
-      const { id, name, email, password, info, braceletId } = user //destructuring
+      const {id, name, email, password, info, braceletId} = user //destructuring
       return (
         <tr key={id}>
           <td>{id}</td>
@@ -46,33 +46,53 @@ class Users extends Component {
           <td>{password}</td>
           <td>{info ?? "null"}</td>
           <td>{braceletId ?? "null"}</td>
-          <td><a style={linkStyle} onClick={ () => {
-            axios.delete("https://localhost:5001/api/users/"+id)
-              .then(this.setState({result: [...this.state.result.filter((x) => x.id
-                  !== id)]})).catch((reason => console.log(reason)))
+          <td><a style={linkStyle} onClick={() => {
+            axios.delete("https://localhost:5001/api/users/" + id)
+              .then(this.setState({
+                result: [...this.state.result.filter((x) => x.id
+                  !== id)]
+              })).catch((reason => console.log(reason)))
           }}>
             delete</a></td>
-          <td><a style={linkStyle} onClick={() => this.setState({idForUpdate: id, updatingDbStarted: true})}>update</a></td>
+          <td><a style={linkStyle} onClick={() => this.setState({idForUpdate: id, updatingDbStarted: true})}>update</a>
+          </td>
         </tr>
       )
     })
   }
 
   renderAddOrUpdateForm = () => {
-    if(!this.state.updatingDbStarted)
+    if (!this.state.updatingDbStarted)
       return;
 
-    return(
-      <React.Fragment>
-        <form onSubmit={this.onSubmit}>
-          <input type="name" placeholder="Name"  onChange={(e) => this.setState({name: e.target.value})}/>
-          <input type="email" placeholder="Email"  onChange={(e) => this.setState({email: e.target.value})} />
-          <input type="password" placeholder="Password"  onChange={(e) => this.setState({password: e.target.value})} />
-          <input type="info" placeholder="Info"  onChange={(e) => this.setState({info: e.target.value})} />
-          <input type="braceletId" placeholder="BraceletId"  onChange={(e) => this.setState({braceletId: e.target.value})} />
-          <input type="submit" />
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className="container">
+          <div className="form-group">
+            <input className="form-control" type="name" placeholder="Name"
+                   onChange={(e) => this.setState({name: e.target.value})}/>
+          </div>
+          <div className="form-group">
+            <input className="form-control" type="email" placeholder="Email"
+                   onChange={(e) => this.setState({email: e.target.value})}/>
+          </div>
+          <div className="form-group">
+            <input className="form-control" type="password" placeholder="Password"
+                   onChange={(e) => this.setState({password: e.target.value})}/>
+          </div>
+          <div className="form-group">
+
+            <input className="form-control" type="info" placeholder="Info"
+                   onChange={(e) => this.setState({info: e.target.value})}/>
+          </div>
+          <div className="form-group">
+            <input className="form-control" type="braceletId" placeholder="BraceletId"
+                   onChange={(e) => this.setState({braceletId: e.target.value})}/>
+          </div>
+          <input className="btn btn-primary" type="submit"/>
+
         </form>
-      </React.Fragment>
+      </div>
     )
   }
 
@@ -89,17 +109,17 @@ class Users extends Component {
     console.log(valuesToSend);
 
     let idForUpdate = this.state.idForUpdate;
-    if (idForUpdate == null){
+    if (idForUpdate == null) {
       axios.post("https://localhost:5001/api/users", valuesToSend)
         .then(res =>
           this.setState({result: [...this.state.result, res.data], idForUpdate: null, updatingDbStarted: false})
         ).catch((reason => console.log(reason)));
-    }
-    else{
+    } else {
       axios.post(`https://localhost:5001/api/users/${idForUpdate}`, valuesToSend)
         .then(res =>
-          this.setState({result: this.state.result.map((val) => {
-              if(val.id === idForUpdate){
+          this.setState({
+            result: this.state.result.map((val) => {
+              if (val.id === idForUpdate) {
                 val.name = valuesToSend.name;
                 val.email = valuesToSend.email;
                 val.password = valuesToSend.password
@@ -108,24 +128,25 @@ class Users extends Component {
               }
 
               return val;
-            }), idForUpdate: null, updatingDbStarted: false})
+            }), idForUpdate: null, updatingDbStarted: false
+          })
         ).catch(reason => console.log(reason));
     }
   }
 
   render() {
     return (
-      <React.Fragment>
+      <div className="container-fluid">
         <h1>Users</h1>
-        <table>
+        <table className="table table-striped  m-md-2">
           <tbody>
-            <tr>{this.renderTableHeader()}</tr>
-            {this.renderTableData()}
+          <tr>{this.renderTableHeader()}</tr>
+          {this.renderTableData()}
           </tbody>
         </table>
-        <button onClick={() => this.setState({updatingDbStarted: true})}>Add</button>
+        <button className="btn-sm" onClick={() => this.setState({updatingDbStarted: true})}>Add new item</button>
         {this.renderAddOrUpdateForm()}
-      </React.Fragment>
+      </div>
     );
   }
 }
